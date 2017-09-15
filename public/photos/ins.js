8141ce4772da674a8d77a5847e335db3821e0602
丁洋 <yang.ding@99bill.com>
2017-09-15 12:25:03 +0800

    修改相册

--- public/photos/ins.js
+++ public/photos/ins.js
@@ -169,9 +169,9 @@
       function loadData(success) {
         if (!searchData) {
           var xhr = new XMLHttpRequest();
-          
-          xhr.open('GET', 'https://github.com/lovexinforever/blog_back_up/blob/master/photos/data.json?t=' + +new Date(), true);
           xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+          xhr.open('GET', 'https://github.com/lovexinforever/blog_back_up/blob/master/photos/data.json?t=' + +new Date(), true);
+  
           xhr.onload = function() {
             if (this.status >= 200 && this.status < 300) {
               var res = JSON.parse(this.response);
